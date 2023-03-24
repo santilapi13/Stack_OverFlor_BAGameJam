cb05ca55bd4ecd230bb7f497a00a30bf8a4fa0fa
santilapi13 <santilapiana02@gmail.com>
2023-03-24 14:11:18 -0300

    Money with text output and GameController singleton finished

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,22 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+    
+    public static MonoBehaviour _instance = null;
+    public Text moneyOutput = null;
+    private int money = 0;
+    private int wordMultiplier = 2;
 
-    public static GameController instance;
-    private int money;
+    public int getMoney() {
+        return money;
+    }
+    
+    public void addMoney(int newMoney) {
+        money += newMoney;
+        updateMoney();
+    }
 
-    void Awake() {
-        if (instance == null)
-            instance = this;
-        else if (instance != this)
-            Destroy(gameObject);
+    public void addMoney(string word) {
+        money += word.Length * this.wordMultiplier;
+        updateMoney();
+    }
+    
+    public void removeMoney(int newMoney) {
+        money -= newMoney;
+        updateMoney();
+    }
+
+    private void updateMoney() {
+        moneyOutput.text = "Dinero: " + money;
+    }
+    
+    public static MonoBehaviour Instance() {
+        if (_instance == null) {
+            _instance = GameObject.FindObjectOfType<GameController>();
+        }
+        return _instance;
     }
 
     // Start is called before the first frame update
     void Start(){
-        
+        updateMoney();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Typer.cs
+++ Assets/Scripts/Typer.cs
@@ -3,86 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Typer : MonoBehaviour
-{
+public class Typer : MonoBehaviour {
+    
     public Text wordOutput = null;
     private string remainingWord = string.Empty;
     private string[] nextWord = {"hello world", "if else", "example code", "generic code", "crustaceo", "test"};
     private string currentWord = string.Empty;
+    
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         setNextWord();
     }
 
        // Update is called once per frame
-    void Update()
-    {
-        checkImput();
+    void Update() {
+        checkInput();
     }
 
-    private void setNextWord()
-    {
+    private void setNextWord() {
        setRemainWord(nextWord[Random.Range(0, nextWord.Length)]);
        setCurrenWord(remainingWord);
     }
     
-
-    private void setCurrenWord(string newString)
-    {
+    
+    private void setCurrenWord(string newString) {
         currentWord = newString;
     }
 
-    private void setRemainWord(string newString)
-    {
+    /**
+     * It sets the remaining word to the new string.
+     * @param newString: The new string to set.
+     */
+    private void setRemainWord(string newString) {
         remainingWord = newString;
         wordOutput.text = remainingWord;
     }
 
-    private void checkImput()
-    {
-        if(Input.anyKeyDown)
-        {
+    /**
+     * It checks if the user has pressed a key and if it is, it checks if only one key is pressed (in the same frame).
+     * If only one is pressed, it calls the enterLetter method to check if the letter is correct.
+     */
+    private void checkInput() {
+        if(Input.anyKeyDown) {
             string keyPressed = Input.inputString;
-            if(keyPressed.Length == 1)
-            {
-               enterLetter(keyPressed); 
-               if(isWordComplete())
-               {
-                   setNextWord();
-               }
-            }
+            if (keyPressed.Length == 1)
+               enterLetter(keyPressed);
         }
     }
 
-    private void enterLetter(string letter)
-    {
-        if(isCorrectLetter(letter))
-        {
+    /**
+     * It checks if the letter is correct and if it is, it removes it from the word.
+     * @param letter: The letter to check.
+     */
+    private void enterLetter(string letter) {
+        if(isCorrectLetter(letter)) {
             removeLetter();
-            if(isWordComplete())
-            {
+            if (isWordComplete()) {
+                ((GameController) GameController.Instance()).addMoney(this.currentWord);
                 setNextWord();
-                
             }
-        }else{
+        } else
             setRemainWord(currentWord);
-        }
     }
     
-    private bool isCorrectLetter(string letter)
-    {
+    /**
+     * It checks if the letter is the next letter of the word.
+     * @param letter: The letter to check.
+     * @return True if the letter is the next letter of the word, false otherwise.
+     */
+    private bool isCorrectLetter(string letter) {
         return remainingWord.IndexOf(letter) == 0;
     }
 
-    private void removeLetter()
-    {
+    /**
+     * It removes the next letter of the word.
+     */
+    private void removeLetter() {
         string newString = remainingWord.Remove(0, 1);
         setRemainWord(newString);
     }
 
-    private bool isWordComplete()
-    {
+    /**
+     * It checks if the word was fully typed.
+     * @return True if the word was fully typed, false otherwise.
+     */
+    private bool isWordComplete() {
         return remainingWord.Length == 0;
     }
 
